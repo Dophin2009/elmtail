ada14025c0be5d2409d620cfd06db6d4e80cacab
Eric Zhao <21zhaoe@protonmail.com>
2022-01-26 10:55:34 -0500

    feat(modules/ansible/ri): Reinstall roles when requirements file changes

--- modules/ansible-role-install/main.tf
+++ modules/ansible-role-install/main.tf
@@ -1,4 +1,8 @@
 resource "null_resource" "install" {
+  triggers = {
+    file_changed = md5(file(var.requirements))
+  }
+
   provisioner "local-exec" {
     command = "ansible-galaxy install -gfr ${var.requirements}"
   }
